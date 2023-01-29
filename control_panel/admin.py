3ae594cd8a6fa8d22202031c6bbb7becf9b74be9
s.leontev <neonyan15@gmail.com>
2023-01-29 20:28:50 +0300

    change status for color, add authority urls and control panel view

--- control_panel/admin.py
+++ control_panel/admin.py
@@ -5,6 +5,6 @@ from control_panel.models import TaskResult
 
 @admin.register(TaskResult)
 class ResultTaskAdmin(admin.ModelAdmin):
-    list_display = ('date', 'full_name', 'status',)
-    search_fields = ('status',)
-    list_filter = ('status', 'date',)
+    list_display = ('date', 'full_name', 'color',)
+    search_fields = ('color',)
+    list_filter = ('color', 'date',)
